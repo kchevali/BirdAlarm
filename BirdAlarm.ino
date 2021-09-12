a5fb8f3ce43c38554c68bb0b900131bca4e17831
Kevin Chevalier <kevin.chevalier10@gmail.com>
2021-09-11 20:06:32 -1000

    BirdAlarmv2 - added timers

--- BirdAlarm.ino
+++ BirdAlarm.ino
@@ -4,14 +4,14 @@
  * - Distance between sensors to detect birds instead of larger things
  */
 
-constexpr int refreshRate = 10000;
-int refreshIndex = 0; //callibration cycle - connects to maxDist in sensor
-long long clck = 0;
-int trigGap = 400; // num of frames before and after only bottom is triggered
-int trigWait = 150;
+#define print(value) Serial.println(value)
+#define printStr(str) Serial.println((String)str)
 
-constexpr int calPin = 6;
-constexpr int ledLength = 5;
+int freeRam () {
+  extern int __heap_start, *__brkval; 
+  int v; 
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
+}
  
 void swap(float& a, float& b){
   float t = a;
@@ -31,101 +31,170 @@ void sort(float* arr, int a, int b){
   
 }
 
+template<int stateCount>
+struct Timer {
+  unsigned long durations[stateCount];
+  unsigned int stateIdx;
+  unsigned long startTime;
+  unsigned long nowTime;
+  bool locked;
+
+  template<typename... Ints>
+  Timer(Ints... durations) : durations{durations...}, stateIdx(0), startTime(0), nowTime(0), locked(false) {}
+
+  void start(){
+    startTime = now();
+  }
+
+  bool stop(bool resetTimer=true){
+    if(!locked)nowTime = now();
+    if(startTime > nowTime){
+      start();
+    }else if(isRunning() && nowTime - startTime >= durations[stateIdx]){
+      if(resetTimer){
+        reset();
+        stateIdx = (stateIdx + 1) % stateCount;
+      }
+      return true;
+    }
+    return false;
+  }
+
+  unsigned long now(){
+    auto t = millis();
+    return t <= 0 ? 1 : t; // makes sure to never return 0 (unlikely case of overflow followed by reading)
+  }
+
+  bool isRunning(){
+    return startTime > 0;
+  }
+
+  void reset(){
+    startTime = 0;
+  }
+
+  void lock(){
+    nowTime = now();
+    locked = true;
+  }
+
+  String status(){
+    return (startTime == 0 ? (String)"Not running" : (String)"Rem: " + (durations[stateIdx] - now() + startTime) + " ms" ) + " [" + stateIdx + "]";
+  }
+};
+
+struct BiTimer: public Timer<2>{
+  template<typename... Args>
+  BiTimer(Args... args): Timer(args...){}
+  bool isLow(){ return stateIdx == 0; }
+  bool isHigh() { return stateIdx == 1; }
+};
+
+struct TriTimer: public Timer<3>{
+  template<typename... Args>
+  TriTimer(Args... args): Timer(args...){}
+  bool isLoad() { return stateIdx == 0; }
+  bool isHigh() { return stateIdx == 1; }
+  bool isLow() { return stateIdx == 2; }
+  void setLoad() { stateIdx = 0, reset(); }
+};
+
+constexpr int calPin = 6;
+BiTimer calTimer(
+  5000UL /* calibration time */,
+  100UL /* LED time */
+);
+
 struct USSensor{
 
   String sensorName;
-  int echoPin, trigPin, detectPin;
-  long duration;
-  float distance;
-  long long lastTrig;
-
-  constexpr static int sampleSize = 20;
-  float distSample[sampleSize];
-  float median = 0;
-  int slot = 0;
-
+  int echoPin, trigPin, detectPin; // arduino pins to the sensor
+  float distance; // median distance read from sensor
   float maxDist = 500, newMaxDist = 0; 
-  int triggerIndex = 0, cooldownIndex = 0;
-  bool firstTriggerRound = false;
+  unsigned long lastTrig; // time in millis since last trig
+  bool updateLastTrig; // can update lastTrig
 
-  constexpr static float threshold = 0.5;
-  constexpr static int cooldownRate = 50;
+  constexpr static int sampleSize = 5; // num sample to calc median distance
+  float distSample[sampleSize]; //samples for median distance calc
+  int slot = 0; // index for next raw distance value in median calc
+
+  constexpr static float maxDistRatio = 0.5;
   constexpr static float maxDistanceAllowed = 1000;
 
-  USSensor(String sensorName, int trigPin, int echoPin, int detectPin): sensorName(sensorName), echoPin(echoPin), trigPin(trigPin), detectPin(detectPin){
+  TriTimer triggerTimer;
+
+  USSensor(String sensorName, int trigPin, int echoPin, int detectPin):
+    sensorName(sensorName), echoPin(echoPin), trigPin(trigPin), detectPin(detectPin),
+    triggerTimer(3000UL /* wait */, 100UL /* LED */, 5000UL /* cooldown */), updateLastTrig(true)
+    {
     pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
-    pinMode(detectPin, OUTPUT); // Sets the trigPin as an OUTPUT
+    pinMode(detectPin, OUTPUT); // Sets the detectPin as an OUTPUT
     pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+
+    digitalWrite(trigPin, LOW);
   }
 
   void update(){
     // Clears the trigPin condition
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
     digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
+    delayMicroseconds(2);
     digitalWrite(trigPin, LOW);
-    // Reads the echoPin, returns the sound wave travel time in microseconds
-    duration = pulseIn(echoPin, HIGH);
-    // Calculating the distance
-    distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+    read();
+  }
 
-    if(distance > maxDistanceAllowed)
+  void read(){
+    // Calculating the distance
+    float rawDistance = pulseIn(echoPin, HIGH) * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+    if(rawDistance > maxDistanceAllowed)
       return;
 
-    distSample[slot] = distance;
+    // get the median distance from previous measurements
+    distSample[slot] = rawDistance;
     slot = (slot + 1) % sampleSize;
+    distance = getMedianDistance();
 
-    updateMedian();
-
-    //update max distance if its callibration
-    newMaxDist = distance > newMaxDist ? distance : newMaxDist;
-    if(refreshIndex == 0){
-      maxDist = newMaxDist;
+    //update max distance if its callibration time
+    newMaxDist = rawDistance > newMaxDist ? rawDistance : newMaxDist;
+    // printStr(sensorName + ": " + (calTimer.stop(false) ? "STOP" : "--") + " " + (calTimer.isHigh() ? "HIGH" : "LOW"));
+    if(calTimer.stop(false) && calTimer.isHigh()){
+      printStr(sensorName + ": CALIBRATED!");
+      maxDist = newMaxDist * maxDistRatio;
       newMaxDist = 0;
     }
 
-    if(isTriggered()){
-      triggerIndex = (triggerIndex + 1) % ledLength;
-      lastTrig = clck;
-    }else if(triggerIndex > 0 && triggerIndex < (ledLength - 1)){
-      triggerIndex = ledLength - 1;
-    }else{
-      triggerIndex = 0;
+    if(triggerTimer.stop()){ // timer ended: go to next stage
+      if(!triggerTimer.isLoad()){ // on and cooldown
+        triggerTimer.start();
+      }
+      trigger();
+    }else if(distance < maxDist){
+      if(triggerTimer.isLoad() && !triggerTimer.isRunning()){
+        triggerTimer.start();
+      }
+    }else if(triggerTimer.isLoad()){
+      triggerTimer.reset();
     }
   }
 
-  void finalUpdate(){
-    if(isStartTriggered()){
-      cooldownIndex = 1;
-    }else if (cooldownIndex > 0){
-      cooldownIndex = (cooldownIndex + 1) % cooldownRate;
+  void trigger(){
+    if(triggerTimer.isHigh()){
+      printStr("##" + sensorName + " triggered##");
+      digitalWrite(detectPin, HIGH);
+      if(updateLastTrig) lastTrig = triggerTimer.now();
+    }else if(triggerTimer.isLow()){
+      digitalWrite(detectPin, LOW);
     }
   }
 
-  bool isTriggered(){
-    return median < (maxDist * threshold);
-  }
-
-  bool isStartTriggered(){
-    return triggerIndex == 1 && cooldownIndex == 0;
-  }
-
-  bool isEndTriggered(){
-    return triggerIndex == ledLength - 1;
-  }
-
-  void updateMedian(){
+  float getMedianDistance(){
     float sortSample[sampleSize];
     memcpy(sortSample, distSample, sizeof(distSample));
     sort(sortSample, 0, sampleSize - 1);
-    median = sortSample[sampleSize/2];
+    return sortSample[sampleSize/2];
   }
 
-  void print() const{
-    Serial.print(median);
-    Serial.print(" cm ");
+  String status(){
+    return sensorName + " | " + distance + " / " + maxDist + " " + newMaxDist + " cm | Timer: " + triggerTimer.status();
   }
 };
 
@@ -136,86 +205,86 @@ USSensor sensors[2] = {
 
 struct ActiveBuzzer{
   int buzzPin;
-  long long lastTrigBottom;
-  bool recentNotTop;
-  ActiveBuzzer(int buzzPin): buzzPin(buzzPin), lastTrigBottom(0), recentNotTop(false){
+  TriTimer buzzTimer;
+
+  constexpr static unsigned long trigGap = 5000; //ms
+  ActiveBuzzer(int buzzPin):
+    buzzPin(buzzPin),
+    buzzTimer(5000UL /* wait */, 20UL /* high */, 50UL /* low */){
     pinMode(buzzPin,OUTPUT);//initialize the buzzer pin as an output
   }
 
   void buzzTrigger(){
     digitalWrite(buzzPin,HIGH);
-    delay(50);//wait for 1ms
-    digitalWrite(buzzPin,LOW);
-    delay(20);//wait for 1ms
+    sensors[0].lastTrig = sensors[1].lastTrig;
   }
 
   void update(){
-    if(sensors[0].lastTrig-sensors[1].lastTrig>= trigGap){ // top has not been triggered recently
-      if(!recentNotTop){
-        recentNotTop = true;
-        lastTrigBottom = sensors[0].lastTrig;
+    if(buzzTimer.stop()){ // timer ended: go to next stage
+      if(!buzzTimer.isLoad()){ // on and cooldown
+        buzzTimer.start();
       }
-      
-      if(clck - lastTrigBottom>=trigWait){ // 
-        for(int i = 0; i < 5; i++) buzzTrigger();
-        sensors[1].lastTrig = clck;
+      buzzTrigger();
+    }else if(sensors[0].lastTrig - sensors[1].lastTrig >= trigGap){
+      if(buzzTimer.isLoad() && !buzzTimer.isRunning()){
+        buzzTimer.start();
       }
-    }else{
-      recentNotTop = false;
+    }else if(buzzTimer.isLoad()){
+      buzzTimer.reset();
     }
-    Serial.println((String)"Sensor Gap: " + (int)(sensors[0].lastTrig - sensors[1].lastTrig) + " Past Gap: " +  (int)(clck - lastTrigBottom));
   }
-  
+
 };
 
 ActiveBuzzer buzzer(9);
 
+int totalTick;
+constexpr int tickSize = 10;
+int ticks[tickSize];
+int tickIdx;
+
 void setup() {  
 
   Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
-  Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
-  Serial.println("with Arduino UNO R3");
+  Serial.println("##Start##");
+
+  totalTick = 0, tickIdx = 0;
+  memset(ticks, 0, sizeof(ticks));
+
+  // printStr("CAL initializing: " + calTimer.status());
+  calTimer.start();
+  // printStr("CAL initialized: " + calTimer.status());
   
 }
 
 void loop() {  
-//  Serial.println((String)"Dist: " + sensors[0].median + " " + sensors[1].median + " MAX: " + sensors[0].maxDist + " " + sensors[1].maxDist+ " CAL: " + (100 * refreshIndex / refreshRate) + "%");
-  
-  //turn on LED when it recallibrates
-  if(refreshIndex == 0){
-    digitalWrite(calPin, HIGH); 
-    Serial.println("CALLIBRATE");  
-  }else if(refreshIndex == ledLength){
-    digitalWrite(calPin, LOW);   
+  int loopStartTime = millis();
+
+  // print("##------------------------------##");
+
+  calTimer.lock();
+
+  for(auto& sensor: sensors) sensor.update();
+  sensors[0].updateLastTrig = sensors[1].distance >= sensors[0].distance;
+  printStr("Sensors: " + sensors[0].status() + " || " + sensors[1].status());
+
+  // buzzer.update();
+
+  if(calTimer.stop()){
+    
+    if(calTimer.isHigh()) printStr("##CALIBRATE##");
+    digitalWrite(calPin, calTimer.isLow() ? LOW : HIGH); 
+    calTimer.start();
   }
-  
-  for(auto& sensor: sensors){
-    sensor.update();
+  else{
+    // printStr("CAL: " + calTimer.status());
   }
 
-//  Serial.println(sensors[0].sensorName + ": trigger = " + sensors[0].triggerIndex);
-//  Serial.print("Distance: ");
-  for(auto& sensor: sensors){
-//    sensor.print();
-    if(sensor.isStartTriggered()){
-      sensor.firstTriggerRound = true;
-      Serial.println(sensor.sensorName + ": TRIGGER - Dist: " + sensor.median + " Max: " + sensor.maxDist + " CAL: " + (100 * refreshIndex / refreshRate) + "%");
-      digitalWrite(sensor.detectPin, HIGH); 
-    }else if(sensor.isEndTriggered() && sensor.firstTriggerRound){
-      sensor.firstTriggerRound = false;
-//      Serial.println(sensor.sensorName + ": END");
-      digitalWrite(sensor.detectPin, LOW); 
-    }
-  }
+  calTimer.locked = false;
 
-  buzzer.update();
-//   Serial.println("");
-
-   for(auto& sensor: sensors){
-    sensor.finalUpdate();
-  }
-//  Serial.println((String)"Refresh: " + refreshIndex);
-  refreshIndex = (refreshIndex + 1) % refreshRate;
-  
-  clck++;
+  totalTick -= ticks[tickIdx];
+  ticks[tickIdx] = millis() - loopStartTime;
+  totalTick += ticks[tickIdx];
+  // printStr("Tick Time: " + (totalTick / tickSize) + " ms | raw: " + ticks[tickIdx]);
+  tickIdx = (tickIdx + 1) % tickSize;
 }
